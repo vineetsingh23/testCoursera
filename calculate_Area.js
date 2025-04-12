75000a4d36db85f7b6a20efc5b69b8fcc0e721ff
Vineet Kumar Singh <vineetsingh.next@gmail.com>
2025-04-12 07:57:33 -0400

    files updated

---
+++ calculate_Area.js
@@ -0,0 +1,6 @@
+let length;
+let width;
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+}
